678ee7db7e484795157e654978376818f5425763
Anas <anasgaamal@gmail.com>
2024-11-02 15:04:55 -0400

    edit background page

--- app/app/layout.js
+++ app/app/layout.js
@@ -23,7 +23,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body>
-        <div className="flex min-h-screen bg-gray-300">
+        <div className="flex min-h-screen bg-gray-100">
           <Sidebar />
           <main className="flex-1">
             {children}
